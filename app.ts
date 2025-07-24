12d6d43c3a59a5c6ba3399a46f345a5553a4182d
jtobey <John.Tobey@gmail.com>
2025-07-24 12:01:36 -0400

    Switched app.ts to use TilesState instead of Bag directly.

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import { Tile } from './tile.js'
-import { createHonorSystemBag } from './honor_system_bag.js'
+import type { TilesState } from './tiles_state.js'
+import { HonorSystemTilesState } from './honor_system_tiles_state.js'
 
-let bag = createHonorSystemBag({tiles: [...Array(100).keys().map(n => new Tile({letter: 'A', value: 1}))], randomSeed: 17})
+let tilesState: TilesState = new HonorSystemTilesState({rackCapacity: 7, tiles: [...Array(100).keys().map(n => new Tile({letter: 'A', value: 1}))], randomSeed: 17, playerIds: ['player1']})
 document.getElementsByTagName('button')[0]?.addEventListener(
-  'click', () => alert(`${bag.size} in bag`))
+  'click', () => alert(`${tilesState.numberOfTilesInBag} in bag`))
