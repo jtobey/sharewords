cf375da27c12d333424a8bc478d9508e5f0cfecc
jtobey <John.Tobey@gmail.com>
2025-08-26 10:45:02 -0400

    Refactored for future swdict support.

--- game/dictionary.ts
+++ game/dictionary.ts
@@ -2,7 +2,6 @@ import { t } from '../i18n.js'
 
 export type Dictionary = ((...possibleWords: Array<string>) => Promise<void>)
 export type DictionaryType = 'permissive' | 'freeapi' | 'custom'
-const DICTIONARY_TYPES: ReadonlyArray<DictionaryType> = ['permissive', 'freeapi', 'custom']
 
 export class PlayRejectedError extends Error {
   constructor(message: string) {
@@ -12,21 +11,23 @@ export class PlayRejectedError extends Error {
 }
 
 export function makeDictionary(dictionaryType: DictionaryType, dictionarySettings: any) {
-  if (!DICTIONARY_TYPES.includes(dictionaryType)) {
-    // Unreachable.
-    throw new Error(`dictionaryType ${dictionaryType} is not supported.`)
+  if (dictionaryType === 'permissive') return async (...words: string[]) => null
+  if (dictionaryType === 'custom') return makeCustomDictionary(dictionarySettings)
+  if (dictionaryType === 'freeapi') {
+    let urlTemplate!: string, dictionaryName!: string
+    if (dictionarySettings) {
+      if (typeof dictionarySettings !== 'string') {
+        throw new Error(`Dictionary type "${dictionaryType}" requires setting a URL template.`)
+      }
+      urlTemplate = dictionarySettings
+      dictionaryName = dictionarySettings
+    } else {
+      urlTemplate = 'https://api.dictionaryapi.dev/api/v2/entries/en/{lower}'
+      dictionaryName = 'Free Dictionary API'
+    }
+    return makeUrlTemplateDictionary(urlTemplate, dictionaryName)
   }
-  return async (...words: Array<string>) => {
-    const promises: Array<Promise<WordNotInDictionaryError | null>> = words.map(word => {
-      return checkWord(word, dictionaryType, dictionarySettings)
-    })
-    const results = await Promise.all(promises)
-    const errors = results.filter(r => r)
-    if (errors.length === 0) return  // Words accepted.
-    if (errors.length === 1) throw errors[0]
-    const invalidWords = errors.map(wnidError => wnidError!.word)
-    throw new PlayRejectedError(t('error.play_rejected.not_words_in_dictionary', { dictionaryName: errors[0]!.dictionaryName, invalidWords: invalidWords.join(', ') }))
-  }
+  throw new Error(`dictionaryType ${dictionaryType} is not supported.`)
 }
 
 class WordNotInDictionaryError extends PlayRejectedError {
@@ -50,51 +51,52 @@ class NoDefinitionError extends WordNotInDictionaryError {
   }
 }
 
+function makeUrlTemplateDictionary(urlTemplate: string, dictionaryName: string) {
+  return async (...words: Array<string>) => {
+    const promises: Array<Promise<WordNotInDictionaryError | null>> = words.map(word => {
+      const url = urlTemplate.replace('{lower}', word.toLowerCase())
+      return checkWordUsingUrl(word, url, dictionaryName)
+    })
+    const results = await Promise.all(promises)
+    const errors = results.filter(r => r)
+    if (errors.length === 0) return  // Words accepted.
+    if (errors.length === 1) throw errors[0]
+    const invalidWords = errors.map(wnidError => wnidError!.word)
+    throw new PlayRejectedError(t('error.play_rejected.not_words_in_dictionary', { dictionaryName: errors[0]!.dictionaryName, invalidWords: invalidWords.join(', ') }))
+  }
+}
+
 /**
  * @returns null if the word is valid.
  * @returns {WordNotInDictionaryError} if `wordToCheck` is not a word according to the dictionary.
  * @throws {Error} on failure to check the word.
  */
-async function checkWord(wordToCheck: string, dictionaryType: DictionaryType, dictionarySettings: any) {
-  if (dictionaryType === 'permissive') return null  // Anything is a word.
-
-  let urlPrefix = ''
-  let dictionaryNameForAlert = ''
-
-  if (dictionaryType === 'freeapi') {
-    urlPrefix = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
-    dictionaryNameForAlert = 'Free Dictionary API'
-  } else if (dictionaryType === 'custom') {
-    dictionaryNameForAlert = 'Custom Dictionary'
-  }
-  if (typeof dictionarySettings === 'string') {
-    urlPrefix = dictionarySettings
-  }
-  if (!urlPrefix) {
-    throw new Error('Custom dictionary requires setting a URL prefix.')
-  }
-
-  const url = urlPrefix + wordToCheck.toLowerCase()
+async function checkWordUsingUrl(wordToCheck: string, url: string, dictionaryName: string) {
   try {
     const response = await fetch(url)
     if (!response.ok) {
       if (response.status === 404) {
-        return new WordNotFoundError(wordToCheck, dictionaryNameForAlert)
+        return new WordNotFoundError(wordToCheck, dictionaryName)
       } else {
-        throw new Error(`Error validating word "${wordToCheck}" with ${dictionaryNameForAlert} (Status: ${response.status} ${response.statusText}).`)
+        throw new Error(`Error validating word "${wordToCheck}" with ${dictionaryName} (Status: ${response.status} ${response.statusText}).`)
       }
     }
-    if (dictionaryType === 'freeapi') {
-      const data = await response.json()
-      if (!Array.isArray(data) || data.length === 0 || (data[0] && data[0].title === "No Definitions Found")) {
-        return new NoDefinitionError(wordToCheck, dictionaryNameForAlert)
-      }
+    const data = await response.json()
+    if (!Array.isArray(data) || data.length === 0 || (data[0] && data[0].title === "No Definitions Found")) {
+      return new NoDefinitionError(wordToCheck, dictionaryName)
     }
     // TODO: Reject words defined only as abbreviations.
-    console.log(`Word "${wordToCheck}" is valid according to ${dictionaryNameForAlert}.`)
+    console.log(`Word "${wordToCheck}" is valid according to ${dictionaryName}.`)
     return null  // Word accepted.
   } catch (error) {
     console.error(`Network or other error validating word "${wordToCheck}":`, error)
-    throw new Error(`Could not reach ${dictionaryNameForAlert} to validate "${wordToCheck}". Check connection or API status.`)
+    throw new Error(`Could not reach ${dictionaryName} to validate "${wordToCheck}". Check connection or API status.`)
+  }
+}
+
+function makeCustomDictionary(dictionarySettings: any) {
+  return async (...words: Array<string>) => {
+    // TODO - fetch swdict, check for words.
+    console.warn(`I want to validiate [${words}] using ${dictionarySettings}.`)
   }
 }
