4267cbd0c3d1b3cce80d5f6aba8a35ff73d7d3bd
jtobey <John.Tobey@gmail.com>
2025-08-26 08:59:19 -0400

    Partial `dictc` compiler CLI.

---
+++ dict/dictc.ts
@@ -0,0 +1,48 @@
+import { parseArgs } from 'util'
+import * as fs from 'fs'
+import * as readline from 'readline'
+import { compile } from './compiler.js'
+
+const { values } = parseArgs({
+  args: Bun.argv.slice(2),
+  options: {
+    'words-file': {
+      type: 'string',
+      short: 'i',
+    },
+    output: {
+      type: 'string',
+      short: 'o',
+    },
+    name: {
+      type: 'string',
+      short: 'n',
+    },
+    description: {
+      type: 'string',
+      short: 'd',
+    },
+  },
+  strict: true,
+});
+
+if (!values['words-file']) throw new Error('No `--words-file` specified.')
+if (!values.output) throw new Error('No `--output` specified.')
+if (!values.name) throw new Error('No `--name` specified.')
+if (!values.description) throw new Error('No `--description` specified.')
+
+const inputStream = fs.createReadStream(values['words-file'])
+
+const words = readline.createInterface({
+  input: inputStream,
+  crlfDelay: Infinity,  // Recognizes both '\n' and '\r\n' as line breaks
+})
+
+const lexicon = await compile({
+  words,
+  name: values.name,
+  description: values.description,
+})
+
+// TODO - write to `values.output`.
+console.log(lexicon)
